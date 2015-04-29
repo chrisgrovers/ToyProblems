13773b5b114ba8d460cb85837ddc398e33d6bfb0
Chris Grovers <chrisgrovers@gmail.com>
2015-04-29 09:56:53 -0700

    implemented my solution

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -12,4 +12,46 @@
 
 var evenOccurrence = function(arr) {
   // Your code here.
+  // store numOccurences with a key(the number) and a value (number of occurences)
+  // var occurrences = [{1:1}];
+  
+  // iterate through the arr, and store the numbers with numOccurence in occurrences
+  // for (var i = 0; i < arr.length; i++) {
+  //   var matched = false;
+  //   // search array for an object with matching key
+  //   if (!matched) {
+  //     for (var j = 0; j < occurrences.length; j++) {
+  //       console.log(occurrences);
+  //       if (occurrences[j])
+  //     }
+  //   } else {
+  //     occurrences[arr[i]] = 1;
+  //   }
+  // }
+  // for (var j = 0; j < occurrences.length; j++) {
+    
+  // }
+  // return occurrences;
+
+  // Your code here.
+  // store numOccurences with a key(the number) and a value (number of occurences)
+  var occurrences = {};
+  
+  // iterate through the arr, and store the numbers with numOccurence in occurrences
+  for (var i = 0; i < arr.length; i++) {
+    if (occurrences[arr[i]]) {
+      occurrences[arr[i]]++;
+    } else {
+      occurrences[arr[i]] = 1;
+    }
+  }
+  for (var key in occurrences) {
+    // iterate through occurrences, looking for an even value stored at key
+    if (occurrences[key] % 2 === 0) {
+      // if it's even, terminate the for loop and console.log the key(which should be the first even occurrence).
+      console.log(key);
+      return;
+    }
+  }
+  // return occurrences;
 };

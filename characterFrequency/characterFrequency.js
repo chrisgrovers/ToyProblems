fda518324d6081f309734df71992ca3d42039dbf
Chris Grovers <chrisgrovers@gmail.com>
2015-05-05 09:54:26 -0700

    counter is working. Sorting is not

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -37,5 +37,37 @@
 
 
 var characterFrequency = function(string) {
+  var holder = {};
+  // iterate through each character in string
+  for (var i = 0; i < string.length; i++) {
+    // push the character as a key, if there is already that key in holder array, add value to it
+    if (holder[string[i]]) {
+      holder[string[i]]++;
+    } else {
+      holder[string[i]] = 1;
+    }
+  }
+  var result = [];
+  for (var key in holder) {
+    // puts all values in holder to result array, for easier sorting
+    console.log(key, holder[key]);
+    result.push([key, holder[key]]);
+  }
+
+  for (var j = 0; j < result.length; j++) {
+    var val = result[j][1];
+    var key = result[j][0];
+    var hole = j;
+    if (result[j-1] && val === result[j-1][1]) {
+      while (hole && key > result[hole - 1][0]) {
+        result[hole] = result[hole-1];
+      }
+    }
+    while (hole && val > result[hole-1][1]) {
+      result[hole] = result[hole-1];
+      hole-=1;
+    }
+  }
+  console.log(result);
   return result;
 };

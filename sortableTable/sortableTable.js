23e0be7c903a819454dd45822007cedce218da76
Chris Grovers <chrisgrovers@gmail.com>
2015-05-21 09:54:49 -0700

    my attempt at sortableTable

--- sortableTable/sortableTable.js
+++ sortableTable/sortableTable.js
@@ -10,6 +10,33 @@
 
 $(function () {
   // TODO: your code here!
+  // select table headers
+  // sort for name
+  $('#myTable thead tr th:first').on('click', function(element){
+    console.log(element);
+    console.log(this);
+    // sort by name
+    // select first child of each tbody tr, and order by name
+    $("#myTable tr td:first-child").
+  });
+  // sort for number of pounds
+  $('#myTable thead tr th:nth-child(2)').on('click', function(element){
+    console.log(element);
+    console.log(this);
+  });
+  // price per pound
+  $('#myTable thead tr th:nth-child(3)').on('click', function(element){
+    console.log(element);
+    console.log(this);
+  });
+  // exp date
+  $('#myTable thead tr th:nth-child(4)').on('click', function(element){
+    console.log(element);
+    console.log(this);
+  });
+
+  // headers on click
+    // function that sorts table
 
 });
 

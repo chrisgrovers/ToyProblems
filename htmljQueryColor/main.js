f3a6f2bb333995886cba567d75d1886464ac7feb
Chris Grovers <chrisgrovers@gmail.com>
2015-07-07 09:20:16 -0700

    [Fix] Added so I can pull from upstream

--- htmljQueryColor/main.js
+++ htmljQueryColor/main.js
@@ -7,12 +7,38 @@ $(function(){
 
   // TODO: your code here!
 
+  $('p').each(function(index) {
+    var words = $(this).html().split(' ')
+    // debugger;
+    for(var i = 0; i < words.length; i++) {
+      // not changing value of words
+      words[i] = '<span>' + words[i] + '</span>';
+    }
+    $(this).html(words.join(' '))
+  });
 
 
   // --------------STEP 2--------------
   // Next, change spans to random colors, once per second
 
   // TODO: your code here!
+  var changeColor = function(span) {
+    var colors = span
+    console.log('colors are', colors)
+    for (var i = 0; i < colors.length; i++) {
+      // add color function?? Not working...
+      colors[i].css('color', function getRandomColor() {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+        })
+    }
+    $(this).html(colors.join(' '));
+  };
 
+  setTimeout(changeColor($('span')), 1000);
 
 });
